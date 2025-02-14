666580bc4aa2c1ff5c8d53c68873fffac3421946
Sunita <sunitamousaviii@gmail.com>
2025-02-14 19:42:26 +0330

    Added My location & fixed changeCity functiom

--- src/index.js
+++ src/index.js
@@ -42,18 +42,40 @@ function updateCityTime() {
 // Dropdown cities
 function changeCity(event) {
   let cityTimeZones = event.target.value;
-  let cityName = cityTimeZones.replace("_", " ").split("/")[1];
-  let cityTime = moment().tz(cityTimeZones);
+
+  if (!cityTimeZones) return; // Prevent empty selection
+
+  if (cityTimeZones === "current-location") {
+    cityTimeZones = moment.tz.guess();
+  }
+
+  let cityName = cityTimeZones.split("/").pop().replace("_", " ");
+  let cityId = cityName.toLowerCase().replace(" ", "-"); // Unique ID based on city name
   let citiesElement = document.querySelector("#cities");
-  citiesElement.innerHTML = `
-  <div class="city" id="london">
-            <div>
-              <h2>${cityName}</h2>
-              <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
-            </div>
-            <div class="time">${cityTime.format("HH:mm:ss")}</div>
-          </div>
-  `;
+  let existingCity = document.querySelector(`#${cityId}`);
+
+  let cityTime = moment().tz(cityTimeZones);
+
+  if (existingCity) {
+    // Update existing city's time instead of adding a duplicate
+    existingCity.querySelector(".date").innerHTML =
+      cityTime.format("MMMM Do YYYY");
+    existingCity.querySelector(".time").innerHTML = cityTime.format("HH:mm:ss");
+  } else {
+    // Add new city if it doesn't already exist
+    citiesElement.insertAdjacentHTML(
+      "beforeend",
+      `
+      <div class="city" id="${cityId}">
+        <div>
+          <h2>${cityName}</h2>
+          <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+        </div>
+        <div class="time">${cityTime.format("HH:mm:ss")}</div>
+      </div>
+      `
+    );
+  }
 }
 
 updateCityTime();
